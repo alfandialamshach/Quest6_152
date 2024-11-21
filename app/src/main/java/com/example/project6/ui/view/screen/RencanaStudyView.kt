8e1e9b96e213c273093afb10861e7d768c915c86
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-21 23:31:51 +0700

    Membyat fungsi RencanaStudyView dan menambahkan variabel chosenDropdwn

--- app/src/main/java/com/example/project6/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/project6/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,19 @@
 package com.example.project6.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import com.example.project6.model.Mahasiswa
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+) {
+    var chosenDropdwn by remember {
+        mutableStateOf(
+            ""
+        )
+    }
+
+}
