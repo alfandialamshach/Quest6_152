f1af5bfe0f5afc72fc92b98f4c4dc83feb960320
Alfandi Alamshach <fandixpc1@gmail.com>
2024-11-22 09:26:41 +0700

    Menambahkan fungsi HasilData

--- app/src/main/java/com/example/project6/ui/view/screen/TampilData.kt
+++ app/src/main/java/com/example/project6/ui/view/screen/TampilData.kt
@@ -3,6 +3,7 @@ package com.example.project6.ui.view.screen
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.content.MediaType.Companion.Text
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -108,5 +109,19 @@ fun TampilData(
 
             }
         }
+    }
+}
+
+@Composable
+fun HasilData(
+    Judul: String,
+    Isinya:String
+){
+    Row(modifier = Modifier.fillMaxWidth()
+        .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween) {
+        Text(Judul, modifier = Modifier.weight(0.8f))
+        Text(":", modifier = Modifier.weight(0.2f))
+        Text(Isinya, modifier = Modifier.weight(2f))
     }
 }
